82d548c1453126ad01799c4628f6bca7bbfe8260
Rodolfo Kong <kongrodolfo@gmail.com>
2025-08-13 08:55:45 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Project Estimated Cost (GTQ)",
+    "summary": "Adds an Estimated Cost field to projects (default GTQ).",
+    "version": "16.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "ARMKU LLC",
+    "website": "www.armku.us",
+    "depends": ["project"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
